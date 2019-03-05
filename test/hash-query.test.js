b730778660eeb719fe91859724f96c51946272ee
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-05 14:06:32 -0800

    Testing for reading from query

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -29,3 +29,43 @@ test('write search to existing query changes search and resets page', assert =>{
     //asssert
     assert.equal(result, 'searchTerm=silent+hill&page=1');
 });
+
+function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+    return searchParams.toString();
+}
+
+test('write page to existing query', assert => {
+    //arrange
+    const existingQuery = 'searchTerm=silent+bob&page=1';
+    const page = 3;
+    //act
+    const result = writePageToQuery(existingQuery, page);
+    const expected = 'searchTerm=silent+bob&page=3';
+    //assert
+    assert.equal(result, expected);
+});
+
+function readFromQuery(queryOptions){
+    const searchParams = new URLSearchParams(queryOptions);
+    const searchObject = {
+        searchTerm: searchParams.get('searchTerm'),
+        page: parseInt(searchParams.get('page'))
+    };
+    return searchObject;
+}
+
+test('reads options from query', assert => {
+    //arrange 
+    //to feed the function, we need querystrings, 
+    const queryOptions = 'searchTerm=silent+bob&page=3';
+    const expected = {
+        searchTerm: 'silent bob',
+        page: 3
+    };
+    //act
+    const result = readFromQuery(queryOptions);
+    //assert
+    assert.deepEqual(result, expected);
+});
